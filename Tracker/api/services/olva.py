63ded291d5e25b8a7ff0fc63c4b606a0c2d8272a
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-09-15 23:06:43 -0500

    Create olva.py

---
+++ Tracker/api/services/olva.py
@@ -0,0 +1,4 @@
+year = 2023
+tracking_number = 1234556
+api_key = a82e5d192fae9bbfee43a964024498e87dfecb884b67c7e95865a3bb07b607dd
+prefix = f'https://reports.olvaexpress.pe/webservice/rest/getTrackingInformation?tracking={tracking_number}&emision={year[-2:]}&apikey={api_key}&details=1'
